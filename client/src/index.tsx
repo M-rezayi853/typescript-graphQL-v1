1b0fb24c59e8e057b4bb386f096cc1b69432ca3e
Mahdi Rezayi <m.rezayi853mh77@gmail.com>
2022-12-31 22:06:40 +0330

    Module 6 Building out Listings

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import reportWebVitals from './reportWebVitals'
+import { Listings } from './sections'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <div>Hello World!</div>
+    <Listings title='TinyHouse Listings' />
   </React.StrictMode>
 )
 
